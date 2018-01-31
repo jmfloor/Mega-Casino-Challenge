4abea04685f018ecec37d8d60563bfb366e67130
Johannes <33619163+jmfloor@users.noreply.github.com>
2018-01-31 09:02:35 +0200

    created a new method to randomise bars changed methods to pass array to check if it is a winning combination or not

--- MegaCasinoChallenge/Default.aspx.cs
+++ MegaCasinoChallenge/Default.aspx.cs
@@ -10,7 +10,7 @@ namespace MegaCasinoChallenge
     public partial class Default : System.Web.UI.Page
     {
 
-       
+        Random randomNumber = new Random();
 
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -25,57 +25,71 @@ namespace MegaCasinoChallenge
 
         protected void leverButton_Click(object sender, EventArgs e)
         {
-            //actions when leverButton is pulled
 
-            //select random pictures for each reel
             
-            spinReels();
 
+            if (!int.TryParse(betTextBox.Text, out int betValue)) return;
+        
+            int multiplier = pullLever();
+            int winnings = multiplier * betValue;
 
-            //determine if it is a winning combinationn
-
-            if (int.TryParse(betTextBox.Text, out int betValue)) ;
-
-                        
-            int multiplier = returnMultiplier() * betValue;
-
-            resultLabel.Text = "You won " + multiplier.ToString();
+            resultLabel.Text = "You won " + winnings.ToString();
 
             int money = int.Parse(ViewState["Money"].ToString());
-            money = money - betValue + multiplier;
+            money = money - betValue + winnings;
 
             ViewState["Money"] = money;
 
-            moneyLabel.Text = money.ToString();
-            
-            //Add viewstate to save money and recall money values
+            resultLabel.Text = (multiplier > 0) ?
+                String.Format("Congratulations, you bet {0:C} and won {1:C}", betValue.ToString(), winnings.ToString())
+                : String.Format("Sorry, you lost {0:C}.", betValue.ToString());
 
-            // update the money
+            moneyLabel.Text = String.Format("Players Money: {0:C}", money.ToString());
 
         }
 
-        private int returnMultiplier()
+        private int pullLever()
         {
-            string[] reelValues = new string[3];
+            string[] reels = new string[] { spinReels(), spinReels(), spinReels() };
 
+            setReelImages(reels);
+
+            return returnMultiplier(reels);
+
+        }
+
+        private void setReelImages(string[] reels)
+        {
+            reelOneImage.ImageUrl = reels[0];
+            reelTwoImage.ImageUrl = reels[1];
+            reelThreeImage.ImageUrl = reels[2];
+        }
+
+        private string spinReels()
+        {
+            string[] sourceImages = new string[12] { "Bar.png", "Bell.png", "Cherry.png", "Clover.png",
+                                                    "Diamond.png", "HorseShoe.png", "Lemon.png",
+                                                    "Orange.png", "Plum.png", "Seven.png", "Strawberry.png",
+                                                    "Watermellon.png" };
+            
+           return "Images/" + sourceImages[randomNumber.Next(0, 11)];
+
+            
+        }
+        private int returnMultiplier(string[] reels)
+        {
+            
             bool isJackPot = true;
-
-            reelValues[0] = reelOneImage.ImageUrl;
-            reelValues[1] = reelTwoImage.ImageUrl;
-            reelValues[2] = reelThreeImage.ImageUrl;
 
             int multiplier = 1;
             
-
             for (int i = 0; i < 3; i++)
             {
-                if (!reelValues[i].Contains("Bar"))
+                if (!reels[i].Contains("Bar"))
                 {
-                    if (reelValues[i].Contains("Cherry"))
-                        multiplier++;
+                    if (reels[i].Contains("Cherry")) multiplier++;
 
-                    if (!reelValues[i].Contains("Seven"))
-                        isJackPot = false;
+                    if (!reels[i].Contains("Seven")) isJackPot = false;
                 }
                 else
                     {
@@ -85,28 +99,10 @@ namespace MegaCasinoChallenge
                     }
             }
 
-            if (isJackPot)
-                           multiplier = 100;
+            if (isJackPot) multiplier = 100;
             
             return multiplier;
         }
-
-        private void spinReels()
-        {
-            string[] sourceImages = new string[12] { "Bar.png", "Bell.png", "Cherry.png", "Clover.png",
-                                                    "Diamond.png", "HorseShoe.png", "Lemon.png",
-                                                    "Orange.png", "Plum.png", "Seven.png", "Strawberry.png",
-                                                    "Watermellon.png" };
-            Random randomNumber = new Random();
-
-            
-            reelOneImage.ImageUrl = "Images/" + sourceImages[randomNumber.Next(0,11)];
-            reelTwoImage.ImageUrl = "Images/" + sourceImages[randomNumber.Next(0, 11)];
-            reelThreeImage.ImageUrl = "Images/" + sourceImages[randomNumber.Next(0, 11)];
-
-            
-        }
-
         
     }
 }
